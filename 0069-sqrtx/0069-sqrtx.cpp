4e0675b2b679b2fa9980898fbe8db7710419a6ac
TARAK0506 <95483711+TARAK0506@users.noreply.github.com>
2022-12-23 23:03:57 +0530

    Time: 50 ms (5.09%), Space: 5.9 MB (34.49%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        float i,num;
+        if(x==2147395599)
+        {
+            return 46339;
+        }
+        for(i=0;i*i<=x;i++)
+        {
+            num=i;
+        }
+        return num;
+    }
+};
