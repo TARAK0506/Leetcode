4c48e16d81cf2147c03337ad8d31ae9a3b169a4d
TARAK0506 <95483711+TARAK0506@users.noreply.github.com>
2023-01-02 22:33:49 +0530

    Time: 5 ms (47.83%), Space: 5.8 MB (75.98%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+     for(long long int i=0;i*i<=c;i++)
+     {
+         double j=sqrt(c-i*i);
+         if(j==(int)j)
+         {
+             return true;
+         }
+     }
+      return false;
+    }
+};
