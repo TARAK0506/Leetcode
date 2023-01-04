cfbb5ea1134a684f347af2d0496ccfb1d701262e
TARAK0506 <95483711+TARAK0506@users.noreply.github.com>
2023-01-04 15:15:27 +0530

    Time: 9 ms (54.40%), Space: 11 MB (43.89%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        for(auto it:nums)
+        {
+            if(i==0 || i==1 || nums[i-2]!=it)
+            {
+                nums[i]=it;
+                i+=1;
+            }
+        }
+        return i;
+    }
+};
