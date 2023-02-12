600b4203ec8f60094b5eb282a32acb96a90aba3e
TARAKA RAMA RAO <95483711+TARAK0506@users.noreply.github.com>
2023-02-12 19:20:15 +0530

    Time: 7 ms (9.13%), Space: 6.1 MB (6.27%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        vector<int> vec{2,3,5};
+        if(n<1)
+        {
+            return false;
+        }
+        for(auto it:vec)
+        {
+            while(n%it==0)
+            {
+                n=n/it;
+            }
+        }
+        if(n==1)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+};
