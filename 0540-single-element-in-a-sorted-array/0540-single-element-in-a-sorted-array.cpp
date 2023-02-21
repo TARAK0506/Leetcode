55debc2e801a01c046cff25c4e36446c4506afb2
TARAKA RAMA RAO <95483711+TARAK0506@users.noreply.github.com>
2023-02-21 16:51:42 +0530

    Time: 70 ms (6.08%), Space: 28.2 MB (6.23%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
+        int ans=0;
         map<int,int>mp;
         for(auto i:nums)
         {
             mp[i]++;
         }
-        int ans;
         for(auto it:mp)
         {
             if(it.second==1)
             {
-                ans=it.first;
+                ans= it.first;
             }
         }
         return ans;
