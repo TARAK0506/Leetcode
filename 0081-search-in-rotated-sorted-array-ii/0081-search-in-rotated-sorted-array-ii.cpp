d600f2d7014cc0c80983efd8a9d25317aac4627a
TARAKA RAMA RAO <95483711+TARAK0506@users.noreply.github.com>
2023-03-11 19:13:57 +0530

    Time: 4 ms (76.00%), Space: 13.9 MB (94.90%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]==target)
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int low=0,high=n-1,mid;
+        while(low<=high){
+            mid=low+(high-low)/2;
+            if(nums[mid]==target)
             {
                 return true;
+            }
+            else if(nums[mid]<target)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
             }
         }
         return false;
