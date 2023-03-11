0b43818344126099be5438a5b92c1271d786b655
TARAKA RAMA RAO <95483711+TARAK0506@users.noreply.github.com>
2023-03-11 21:44:05 +0530

    Time: 37 ms (34.91%), Space: 25.7 MB (12.46%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -6,10 +6,9 @@ public:
             k=k%n;
         }
         vector<int> arr(n);
-        int j=0;
         for(int i=n-k;i<n;i++)
         {
-            arr[j++]=nums[i];
+            arr[i-(n-k)]=nums[i];
         }
         for(int i=0;i<n-k;i++)
         {
