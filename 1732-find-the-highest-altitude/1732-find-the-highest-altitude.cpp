9d1d2a57e3311e6e9500f74fa42c9b8fee85256a
TARAKA RAMA RAO <95483711+TARAK0506@users.noreply.github.com>
2023-05-09 14:23:40 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (5.74%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        int sum=0,n=gain.size();
-        vector<int>prefixsum;
-        prefixsum.push_back(0);
-        for(auto it:gain){
-            sum+=it;
-            prefixsum.emplace_back(sum);
+        vector<int>prefix(gain.size());
+        prefix[0]=gain[0];
+        for(int i=1;i<gain.size();i++){
+            prefix[i]=gain[i]+prefix[i-1];
         }
-        sort(prefixsum.rbegin(),prefixsum.rend());
-        return prefixsum[0];
+        prefix.emplace_back(0);
+        sort(prefix.rbegin(),prefix.rend());
+        return prefix[0];
     }
 };
