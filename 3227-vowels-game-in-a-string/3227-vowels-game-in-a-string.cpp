bdd163b73e33ca74bad095b3d791f0ac483d5c29
TARAKA RAMA RAO <tarakaramarao0506@gmail.com>
2024-08-06 21:01:45 +0530

    Time: 39 ms (89.08%), Space: 17.5 MB (76.19%) - LeetHub

---
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+      int vowels=0;
+      for(auto ch:s){
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+          vowels++;
+        }
+      }
+      if(vowels==0) return false;
+        return true;
+    }
+};
