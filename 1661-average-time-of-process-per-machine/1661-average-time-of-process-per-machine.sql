5f822372abfaab4a2a5b3bb154545a3b09d640bf
TARAKA RAMA RAO <tarakaramarao0506@gmail.com>
2024-08-19 20:58:21 +0530

    Time: 246 ms (24.30%), Space: 0B (100.00%) - LeetHub

---
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,1 @@
+SELECT A1.machine_id ,ROUND(AVG(A1.timestamp - A2.timestamp ),3) AS 'processing_time' FROM Activity AS A1 JOIN Activity AS A2 ON A1.machine_id = A2.machine_id AND A1.process_id = A2.process_id AND A2.timestamp < A1.timestamp GROUP BY A1.machine_id;
