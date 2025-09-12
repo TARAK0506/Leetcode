438ce222097724f1d32fb60951cdbbf429592c48
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2025-09-12 14:21:56 +0530

    Time: 0 ms (100%), Space: 8.5 MB (84.8%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        float i,num;
-        if(x==2147395599)
-        {
-            return 46339;
+        long long ans = 0;
+        long long low = 1, high = x;
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+            if (mid * mid <= x) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
-        for(i=0;i*i<=x;i++)
-        {
-            num=i;
-        }
-        return num;
+        return ans;
     }
 };
