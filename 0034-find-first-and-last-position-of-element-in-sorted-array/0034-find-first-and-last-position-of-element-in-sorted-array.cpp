9efeec977de679fdba2057189f146ff779f15635
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2025-09-12 21:14:22 +0530

    Time: 0 ms (100%), Space: 17.4 MB (99.33%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,19 +1,41 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans;
-        int first = -1, last = -1;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == target) {
-                if (first == -1) {
-                    first = i;
-                }
-                last = i;
-            }
+    int firstOccurence(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        int first = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                first = mid;
+                high = mid - 1;
+            } else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
-        if (first == -1 && last == -1)
-            return {-1, -1};
-        else
-            return {first, last};
+        return first;
+    }
+    int lastOccurence(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        int last = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                last = mid;
+                low = mid + 1;
+            } else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return last;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first = firstOccurence(nums, target);
+        int last = lastOccurence(nums, target);
+
+        return {first, last};
     }
 };
