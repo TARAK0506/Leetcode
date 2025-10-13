a80f9d0cb4759137901aebcb635a6ad58bb5d02b
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2025-10-13 15:14:09 +0530

    Time: 11 ms (11.27%), Space: 21.3 MB (7.66%) - LeetHub

---
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isAnagram(string a, string b) {
+        if (a.length() != b.length())
+            return false;
+        unordered_map<char, int> mp;
+        for (auto& ch : a) {
+            mp[ch]++;
+        }
+        for (auto& ch : b) {
+            mp[ch]--;
+            if (mp[ch] < 0)
+                return false;
+        }
+        return true;
+    }
+    vector<string> removeAnagrams(vector<string>& words) {
+        int n = words.size();
+        vector<string> ans;
+        ans.emplace_back(words[0]);
+        for(int i = 1; i < n; i++){
+            if(!isAnagram(words[i - 1], words[i])){
+                ans.emplace_back(words[i]);
+            }
+        }
+        return ans;
+    }
+};
