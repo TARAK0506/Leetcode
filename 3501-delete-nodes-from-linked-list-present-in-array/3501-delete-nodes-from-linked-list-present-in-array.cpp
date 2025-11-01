cfbb26e4202f7202904445741605f2b7794968e4
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2025-11-01 12:16:41 +0530

    Time: 98 ms (62.22%), Space: 263.2 MB (86.67%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
@@ -15,16 +15,16 @@ public:
         for (auto& num : nums) {
             s.insert(num);
         }
-        ListNode* dummy = new ListNode(-1);
-        dummy->next = head;
-        ListNode* curr = dummy;
-        while (curr->next) {
-            if (s.count(curr->next->val)) {
+        while (head && s.find(head->val) != s.end())
+            head = head->next;
+        ListNode* curr = head;
+        while (curr && curr->next) {
+            if (s.find(curr->next->val) != s.end()) {
                 curr->next = curr->next->next;
             } else {
                 curr = curr->next;
             }
         }
-        return dummy->next;
+        return head;
     }
 };
