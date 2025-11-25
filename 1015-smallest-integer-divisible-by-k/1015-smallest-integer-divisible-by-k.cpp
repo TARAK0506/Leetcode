a8d9e722aaad99e55742a0e347c8cd9f0bad5f3a
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2025-11-25 17:23:37 +0530

    Time: 0 ms (100%), Space: 7.7 MB (81.5%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
@@ -4,7 +4,7 @@ public:
         int cnt = 1, rem = 1;
         if (k == 1)
             return 1;
-        if (k % 2 == 0 || k % 5 == 0)
+        if (k % 2 == 0 || k % 5 == 0 || k % 6 == 0)
             return -1;
         for (int i = 1; i <= k; i++) {
             cnt++;
