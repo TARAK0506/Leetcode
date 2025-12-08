95a6b20feb665f3746ce2d28b92a8e03de7c8c72
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2025-12-08 11:17:09 +0530

    Time: 125 ms (24.12%), Space: 7.9 MB (21.68%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPerfectSquare(int c) {
+        int low = 1, high = c;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (mid * mid == c) {
+                return true;
+            } else if (mid * mid < c) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+    int countTriples(int n) {
+        int cnt = 0;
+        for (int a = 1; a <= n; a++) {
+            for (int b = 1; b <= n; b++) {
+                int c = a * a + b * b;
+                if (c <= n * n && isPerfectSquare(c)) {
+                    cout << a << " " << b << " " << c << "\n";
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
