dba2ecf7b68b712d4b84e3dbc6c67379691d2a6d
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2026-01-28 09:18:58 +0530

    Time: 0 ms (100%), Space: 16.1 MB (83.55%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -6,28 +6,28 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int sumOfLeftLeaves(TreeNode* root) {
-        if(!root) return 0;
-        int sum = 0;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-          int size = q.size();
-          for(int i=0;i<size;i++){
-            TreeNode* curr = q.front();
-            q.pop();
-            if(curr->left && !curr->left->left && !curr->left->right){
-              sum+=curr->left->val;
-            }
-            if(curr->left) q.push(curr->left);
-            if(curr->right) q.push(curr->right);
-          }
+    bool isLeaf(TreeNode* root) {
+        return root && root->left == nullptr && root->right == nullptr;
+    }
+
+    void dfs(TreeNode* root, int& sum) {
+        if (!root)
+            return;
+        if (isLeaf(root->left)) {
+            sum += root->left->val;
         }
+        dfs(root->left, sum);
+        dfs(root->right, sum);
+    }
+    int sumOfLeftLeaves(TreeNode* root) {
+        int sum = 0;
+        dfs(root, sum);
         return sum;
     }
 };
