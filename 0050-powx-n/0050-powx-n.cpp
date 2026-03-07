645697a5260b4f7bc95df8ed39baeb589a00bd9d
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2026-03-07 15:26:15 +0530

    Time: 0 ms (100%), Space: 8.7 MB (14.42%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        double res = 1;
+        long long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -1 * N;
+        }
+        while (N > 0) {
+            if ((N & 1)) {
+                res *= x;
+            }
+            x *= x;
+            N >>= 1;
+        }
+        return res;
     }
 };
