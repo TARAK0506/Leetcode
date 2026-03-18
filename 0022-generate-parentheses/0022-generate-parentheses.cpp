483efde603b8616721bb8090b8c0809827c18376
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2026-03-18 12:54:08 +0530

    Time: 183 ms (5.1%), Space: 193.5 MB (5.04%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<string> ans;
+    bool isValid(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                st.push('(');
+            } else {
+                if (st.empty())
+                    return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+    void dfs(string& curr, int n) {
+        if (curr.length() == 2 * n) {
+            if (isValid(curr))
+                ans.emplace_back(curr);
+            return ;
+        }
+        curr.push_back('(');
+        dfs(curr, n);
+        curr.pop_back();
+
+        curr.push_back(')');
+        dfs(curr, n);
+        curr.pop_back();
+    }
+    vector<string> generateParenthesis(int n) {
+        string curr = "";
+        dfs(curr, n);
+        return ans;
+    }
+};
