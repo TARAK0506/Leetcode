d6ccc80777442972d9b05dceb28c5535e71a1e11
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2026-03-27 22:02:52 +0530

    Time: 134 ms (80.97%), Space: 38.6 MB (79.42%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    vector<int> ds;
-    void backtrack(vector<int>& nums, int k, int idx, vector<int>& ds,
-                   unordered_map<int, int>& mp) {
+    int cnt = 0;
+    void backtrack(vector<int>& nums, int k, int idx, unordered_map<int, int>& mp) {
         if (idx == nums.size()) {
-            if (!ds.empty())
-                ans.emplace_back(ds);
+            if (!mp.empty())
+                cnt++;
             return;
         }
 
-        backtrack(nums, k, idx + 1, ds, mp);
+        backtrack(nums, k, idx + 1, mp);
         if (mp[nums[idx] - k] == 0) {
             mp[nums[idx]]++;
-            ds.emplace_back(nums[idx]);
-            backtrack(nums, k, idx + 1, ds, mp);
+            backtrack(nums, k, idx + 1, mp);
             mp[nums[idx]]--;
-            ds.pop_back();
         }
     }
     int beautifulSubsets(vector<int>& nums, int k) {
         int n = nums.size();
         sort(begin(nums), end(nums));
         unordered_map<int, int> mp;
-        backtrack(nums, k, 0, ds, mp);
-        return ans.size();
+        backtrack(nums, k, 0, mp);
+        return cnt;
     }
 };
