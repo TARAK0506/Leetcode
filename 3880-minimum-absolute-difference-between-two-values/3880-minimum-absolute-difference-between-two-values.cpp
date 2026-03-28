a0813e0cada4bbfe7d671d59f213c4fcf36e5ee1
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2026-03-28 21:41:19 +0530

    Time: 2 ms (24.98%), Space: 25.7 MB (34.52%) - LeetHub

--- 3880-minimum-absolute-difference-between-two-values/3880-minimum-absolute-difference-between-two-values.cpp
+++ 3880-minimum-absolute-difference-between-two-values/3880-minimum-absolute-difference-between-two-values.cpp
@@ -3,8 +3,8 @@ public:
     int minAbsoluteDifference(vector<int>& nums) {
         int n = nums.size(), minDiff = 110;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (nums[i] == 1 && nums[j] == 2) {
+            for (int j = i + 1; j < n; j++) {
+                if ((nums[i] == 1 && nums[j] == 2) || (nums[i] == 2 && nums[j] == 1)) {
                     minDiff = min(minDiff, abs(i - j));
                 }
             }
