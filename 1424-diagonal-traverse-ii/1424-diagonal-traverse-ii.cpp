af8b136da8d6051bab893f8c00d42e29c20bd32b
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2026-03-31 17:21:33 +0530

    Time: 129 ms (31.63%), Space: 103.1 MB (34.95%) - LeetHub

---
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        map<int, vector<int>> mp;
+        for (int i = 0; i < n; i++) {
+            int m = nums[i].size();
+            for (int j = 0; j < m; j++) {
+                mp[i + j].push_back(nums[i][j]);
+            }
+        }
+        for (auto& [key, val] : mp) {
+            if (key & 1 == 0) {
+                for (auto& itr : val) {
+                    ans.emplace_back(itr);
+                }
+            } else {
+                reverse(val.begin(), val.end());
+                for (auto& itr : val) {
+                    ans.emplace_back(itr);
+                }
+            }
+        }
+        return ans;
+    }
+};
