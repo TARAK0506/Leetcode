ccf575d39d7f3946977522a869509cb3a0a51cbb
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2026-06-02 13:34:17 +0530

    Time: 0 ms (100%), Space: 91.6 MB (99.16%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-    int earliestFinishTime(vector<int>& landStartTime,
-                           vector<int>& landDuration,
-                           vector<int>& waterStartTime,
-                           vector<int>& waterDuration) {
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
         int n = landStartTime.size(), m = waterStartTime.size();
         int landFinishTime = INT_MAX, ans = INT_MAX;
         for (int i = 0; i < n; i++) {
-            landFinishTime =
-                min(landFinishTime, (landStartTime[i] + landDuration[i]));
+            landFinishTime = min(landFinishTime, (landStartTime[i] + landDuration[i]));
         }
         int waterFinishTime = INT_MAX;
         for (int j = 0; j < m; j++) {
-            waterFinishTime =
-                min(waterFinishTime, (waterStartTime[j] + waterDuration[j]));
-            ans = min(ans, max(landFinishTime, waterStartTime[j]) +
-                               waterDuration[j]);
+            waterFinishTime = min(waterFinishTime, (waterStartTime[j] + waterDuration[j]));
+            ans = min(ans, max(landFinishTime, waterStartTime[j]) + waterDuration[j]);
         }
 
         for (int i = 0; i < n; i++) {
-            ans = min(ans,
-                      max(waterFinishTime, landStartTime[i]) + landDuration[i]);
+            ans = min(ans, max(waterFinishTime, landStartTime[i]) + landDuration[i]);
         }
         return ans;
     }
