bb431836ab7d4872ab003df5dbfffe77590e8bac
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2026-07-01 22:44:19 +0530

    Time: 236 ms (58.81%), Space: 146.8 MB (49.45%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
+    void merge(vector<int>& nums, int low, int mid, int high) {
+        vector<int> temp((high - low + 1), 0);
+        int idx = 0, left = low, right = mid + 1;
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) {
+                temp[idx++] = nums[left++];
+            } else {
+                temp[idx++] = nums[right++];
+            }
+        }
+        while (left <= mid) {
+            temp[idx++] = nums[left++];
+        }
+        while (right <= high) {
+            temp[idx++] = nums[right++];
+        }
+        for (int i = low; i <= high; i++)
+            nums[i] = temp[i - low];
+    }
+    void mergeSort(vector<int>& nums, int low, int high) {
+        if (low >= high)
+            return;
+        int mid = low + (high - low) / 2;
+        mergeSort(nums, low, mid);
+        mergeSort(nums, mid + 1, high);
+        merge(nums, low, mid, high);
+    }
     vector<int> sortArray(vector<int>& nums) {
-        vector<int>ans;
-        priority_queue <int ,vector<int> ,greater<int>> pq;
-        for(int i=0;i<nums.size();i++)
-        {
-            pq.push(nums[i]);
-        }
-        while(!pq.empty())
-        {
-            ans.push_back(pq.top());
-            pq.pop();
-        }
-        return ans;
+        mergeSort(nums, 0, nums.size() - 1);
+        return nums;
     }
 };
