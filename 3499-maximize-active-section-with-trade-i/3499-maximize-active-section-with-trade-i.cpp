dbf6c6222c8360a53d971150c9f4fc9ff6e07e9f
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2026-07-22 01:03:09 +0530

    Time: 58 ms (68.02%), Space: 80.8 MB (37.39%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        int n = s.length(), cnt = 0, maxx = INT_MIN, cntOnes = 0;
+        vector<int> values;
+        char prev;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '1') {
+                cntOnes++;
+                if (prev == '0')
+                    values.emplace_back(cnt);
+                cnt = 0;
+            } else {
+                cnt -= 1;
+            }
+            prev = s[i];
+        }
+        if (cnt < 0 || prev == '0')
+            values.emplace_back(cnt);
+        int prevv = 1;
+        if (cntOnes == 0 || cntOnes == n)
+            return cntOnes;
+        if (values.size() == 1)
+            return cntOnes;
+        for (int i = 1; i < values.size(); i++) {
+            maxx = max(maxx, abs(values[i - 1] + values[i]));
+        }
+        return maxx + cntOnes;
+    }
+};
