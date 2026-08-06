10ddde0406b75fce0090d4524ef9112e1c6bff2c
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2026-08-06 06:59:07 +0530

    Time: 0 ms (100%), Space: 23.1 MB (21.07%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int height(TreeNode* root) {
+        if (!root)
+            return 0;
+        return 1 + max(height(root->left), height(root->right));
+    }
+    bool solve(TreeNode* root) {
+        if (!root)
+            return true;
+
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+
+        if (abs(leftHeight - rightHeight) > 1)
+            return false;
+        return solve(root->left) && solve(root->right);
+    }
+    bool isBalanced(TreeNode* root) { 
+        return solve(root); 
+    }
+};
