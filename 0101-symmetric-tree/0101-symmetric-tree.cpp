8cf4f89bfff3de735ff9f6256f7b8ed975c3998d
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2026-08-20 00:06:04 +0530

    Time: 0 ms (100%), Space: 18.6 MB (40.71%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -19,7 +19,7 @@ public:
             return false;
         if (r1->val != r2->val)
             return false;
-        return solve(r1->left, r2->right) && solve(r2->left, r1->right);
+        return solve(r1->left, r2->right) && solve(r1->right, r2->left);
     }
     bool isSymmetric(TreeNode* root) {
         if (!root->left && !root->right)
