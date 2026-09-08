9cd4940fa8aee34db2fd402b4f5458d856958ffd
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2026-09-08 09:36:09 +0530

    Time: 291 ms (5.49%), Space: 107.7 MB (5.41%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+*/
+
+class Solution {
+public:
+    int dfs(int id, unordered_map<int, Employee*> mp) {
+        int totalImp = mp[id]->importance;
+        for (auto& subId : mp[id]->subordinates) {
+            totalImp += dfs(subId, mp);
+        }
+        return totalImp;
+    }
+    int getImportance(vector<Employee*> employees, int id) {
+        unordered_map<int, Employee*> mp;
+        // for (auto& employee : employees) {
+        //     cout << employee->id << " " << employee->importance << " ";
+        //     for (int sub : employee->subordinates) {
+        //         cout << sub << " ";
+        //     }
+        //     cout << "\n";
+        // }
+
+        for (auto& employee : employees) {
+            mp[employee->id] = employee;
+        }
+        return dfs(id, mp);
+    }
+};
